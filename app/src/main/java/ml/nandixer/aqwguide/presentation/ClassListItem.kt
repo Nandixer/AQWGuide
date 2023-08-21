175bf87acaac00469790e601ba597b2d0068efb8
MNandor <mnandor@users.noreply.github.com>
2023-08-21 21:54:30 +0100

    ratings background

--- app/src/main/java/ml/nandixer/aqwguide/presentation/ClassListItem.kt
+++ app/src/main/java/ml/nandixer/aqwguide/presentation/ClassListItem.kt
@@ -1,13 +1,16 @@
 package ml.nandixer.aqwguide.presentation
 
+import android.graphics.Point
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.LinearOutSlowInEasing
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -19,11 +22,17 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PaintingStyle.Companion.Stroke
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.PointMode
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import ml.nandixer.aqwguide.domain.model.CombatClass
+import kotlin.math.sqrt
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -55,6 +64,57 @@ fun ClassListItem(theClass: CombatClass, viewModel: MainViewModel){
             }
             if (isExpanded) {
                 Spacer(modifier = Modifier.height(16.dp))
+
+                Canvas(modifier = Modifier.fillMaxWidth(.7f).aspectRatio(1f).align(Alignment.CenterHorizontally)){
+                    //drawCircle(color = Color.Red)
+                    val path = Path().apply {
+                        val cY = size.height/2
+                        val cX = size.width/2
+
+                        drawLine(color = Color.White,
+                            Offset(cX, cY-size.height/2),
+                            Offset(cX, cY+size.height/2),
+                            strokeWidth = 4f
+                        )
+
+                        drawLine(color = Color.White,
+                            Offset(cX+size.width*sqrt(3f)/2f/2, cY-size.height/2/2),
+                            Offset(cX-size.width* sqrt(3f)/2f/2, cY+size.height/2/2),
+                            strokeWidth = 4f
+                        )
+
+                        drawLine(color = Color.White,
+                            Offset(cX+size.width*sqrt(3f)/2f/2, cY+size.height/2/2),
+                            Offset(cX-size.width* sqrt(3f)/2f/2, cY-size.height/2/2),
+                            strokeWidth = 4f
+                        )
+
+                        for (mult in 1..6){
+                            val m = mult/6f
+
+
+
+                            drawPoints(
+                                points = listOf(
+
+                                    Offset(cX, cY-size.height/2*m),
+                                    Offset(cX+size.width*sqrt(3f)/2f/2*m, cY-size.height/2/2*m),
+                                    Offset(cX+size.width*sqrt(3f)/2f/2*m, cY+size.height/2/2*m),
+                                    Offset(cX, cY+size.height/2*m),
+                                    Offset(cX-size.width* sqrt(3f)/2f/2*m, cY+size.height/2/2*m),
+                                    Offset(cX-size.width* sqrt(3f)/2f/2*m, cY-size.height/2/2*m),
+                                    Offset(cX, cY-size.height/2*m),
+
+                                ),
+                                pointMode = PointMode.Polygon,
+                                color = Color.White,
+                                strokeWidth = 4f
+
+                            )
+
+                        }
+                    }
+                }
 
                 Text(text = "Damage: ${ratings.damage}, Survival: ${ratings.survival}, Support: ${ratings.support}")
                 Text(text = "Farming: ${ratings.farming}, PvP: ${ratings.pvp}, Ultras: ${ratings.ultras}")
